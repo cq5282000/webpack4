7b47400f55c1bbf57b3ad66f3a47cb2d534d8c23
chenqu <chenqu@meituan.com>
2018-03-21 17:48:22 +0800

    add

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+var OpenBrowserPlugin = require('open-browser-webpack-plugin');
 
 module.exports = {
     entry: './index.js',
@@ -15,10 +16,22 @@ module.exports = {
             use: 'babel-loader',
         }],
     },
+    devtool: 'cheap-module-eval-source-map',
     plugins: [
         new HtmlWebpackPlugin({
             filename: 'index.html',
             template: 'index.html',
         }),
+        new OpenBrowserPlugin({ url: 'http://localhost:8080' }),
     ],
+    devServer: {
+        // hot: true, // 告诉 dev-server 我们在使用 HMR
+        contentBase: path.resolve(__dirname, 'node_modules'),
+        // inline: true,
+        historyApiFallback: true,
+        // stats: 'normal',
+        // publicPath: publicPathStr,
+        // host: '0.0.0.0',
+        // port: 8000,
+    },
 };
